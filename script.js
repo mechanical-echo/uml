12693f8c36d7e0a813fca5d848c38f6a62321a11
Yulia Zitkova <main.zhitkova@gmail.com>
2024-11-13 01:35:41 +0200

    component foto

--- script.js
+++ script.js
@@ -129,3 +129,15 @@ function showAnswers(id) {
     })
 }
 
+
+
+function showContent() {
+    var content = document.getElementById('content');
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+
+
